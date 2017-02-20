f5cdf5a2e7236069f24b3f817361301645db5af3
Guang Shi <guang@Guangs-MacBook-Pro.local>
2017-02-19 22:46:45 -0600

    update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ ext_modules = [Extension('core/_contactmap', ['src/_contactmap.pyx'], include_di
                Extension('core/_sdp',['src/_sdp.pyx'], include_dirs = [numpy.get_include()]),
                Extension('core/_distmap',['src/_distmap.pyx'], include_dirs = [numpy.get_include()]),
                Extension('core/_contactevolution',['src/_contactevolution.pyx'],include_dirs = [numpy.get_include()]),
-               Extension('core/_loop_gyration_tensor',['src/_loop_gyration_tensor'],include_dirs = [numpy.get_include()])]
+               Extension('core/_loop_gyration_tensor',['src/_loop_gyration_tensor.pyx'],include_dirs = [numpy.get_include()])]
 
 setup(
   ext_modules = cythonize(ext_modules),
