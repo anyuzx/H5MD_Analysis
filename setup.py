ba35134b9d66c6b228a36ff0bdf8c1285c6bc3d7
anyuzx <stefanshi1988@gmail.com>
2017-12-14 12:34:40 -0600

    update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import numpy
 
 ext_modules = [Extension('core/_contactmap', ['src/_contactmap.pyx'], include_dirs = [numpy.get_include()]),
                Extension('core/_sdp',['src/_sdp.pyx'], include_dirs = [numpy.get_include()]),
-               Extension('core/_sdp',['src/_sdp_hist.pyx'], include_dirs = [numpy.get_include()]),
+               Extension('core/_sdp_hist',['src/_sdp_hist.pyx'], include_dirs = [numpy.get_include()]),
                Extension('core/_ps',['src/_ps.pyx'], include_dirs = [numpy.get_include()]),
                Extension('core/_distmap',['src/_distmap.pyx'], include_dirs = [numpy.get_include()]),
                Extension('core/_contactevolution',['src/_contactevolution.pyx'],include_dirs = [numpy.get_include()]),
